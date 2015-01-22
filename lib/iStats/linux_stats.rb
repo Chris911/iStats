140af44ac945b043495281589e8025db41afd06a
Matti-Oskari Leppanen <mongrelx@gmail.com>
2015-01-22 14:57:20 +0200

    better naming

--- lib/iStats/linux_stats.rb
+++ lib/iStats/linux_stats.rb
@@ -1,7 +1,7 @@
 require 'sensors'
 require 'ap'
 
-module A
+module SensorsData 
 @@data=Hash[Sensors.chips.map {|chip|
   [chip.name, Hash[chip.features.map {|feature|
     [feature.label, Hash[feature.subfeatures.map {|subfeature|
@@ -22,17 +22,17 @@ end
 
 end
 module FAN_STATS
-include A
+include SensorsData 
 
 def get_fan_number
-A::data.each do |key, value|
+SensorsData::data.each do |key, value|
 
 return value.count{|k, _| k =~ /fan\d?\z/}
 end
 end
 
 def get_fan_speed(fan)
-A::data.each do |key, value|
+SensorsData::data.each do |key, value|
 begin	
 fan_values=value.fetch("fan#{fan+1}")
 rescue
@@ -48,16 +48,13 @@ end
 end
 
 module CPU_STATS
-include A
+include SensorsData 
 def get_cpu_temp
-	A::data.each do |key, value|
+	SensorsData::data.each do |key, value|
   		temp1= value.fetch("temp1")  
 	return temp1.fetch("temp1_input")
 end
 
 end
 end
-
-include A
-#include CPU_STATS
-#get_cpu_temp
+include SensorsData
