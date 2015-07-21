ac3f01ec51845a4239b369c8f9d439ef86a593b2
Matti-Oskari Leppanen <mongrelx@gmail.com>
2015-07-21 12:23:34 +0300

    Default config(no file) ... i think this way it's better .. no need to compile for default key change

--- lib/iStats/settings.rb
+++ lib/iStats/settings.rb
@@ -9,7 +9,21 @@ module IStats
 
         if File.exists?( @configDir+@configFile )
           $config = ParseConfig.new(@configDir+@configFile)
+        else
+           sensors=Hash.new
+           sensors['thresholds'] = "[50, 68, 80, 90]"
+           name="CPU Die temperature"
+           sensors['name']=name
+           sensors['enabled']="1"
+           key="TC0P"
+           $config =ParseConfig.new
+           $config.add(key,sensors)
+        end
+      end
 
+      def configFileExists
+         if File.exists?( @configDir+@configFile )
+          $config = ParseConfig.new(@configDir+@configFile)
         else
           puts "No config file #{@configDir}#{@configFile} found"
           if !File.exists?(@configDir)
@@ -17,13 +31,9 @@ module IStats
           end
           file=File.open(@configDir+@configFile,"w+")
           file.close
-          puts "Running scan"
-          SMC.scan_supported_keys
-          $config = ParseConfig.new(@configDir+@configFile)
-
         end
       end
-
+  
       def addSensor(key,sensors)
         settings = ParseConfig.new(@configDir+@configFile)
         settings.add(key,sensors)
