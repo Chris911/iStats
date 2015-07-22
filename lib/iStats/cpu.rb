102bda621789584edaa91737c803e3c9b708bd91
Matti-Oskari Leppanen <mongrelx@gmail.com>
2015-07-22 15:34:39 +0300

    Added alternative way to display temps .. as i find it easier to read when  aligned .. also added rounding

--- lib/iStats/cpu.rb
+++ lib/iStats/cpu.rb
@@ -29,11 +29,16 @@ module IStats
       #
       def cpu_temperature        
         sensors =$config.params
+        display=sensors['AltDisplay']
         sensors.keys.each{|key|
           if (sensors[key]['enabled']== "1")
-            t = SMC.is_key_supported(key);
-            thresholds=sensors[key]['thresholds'][1..-2].split(/, /).map { |s| s.to_i }            
-            puts "#{key} #{sensors[key]['name']} temp: #{t}#{Symbols.degree}C  " + Printer.gen_sparkline(t, thresholds)
+            t = SMC.is_key_supported(key).round(2);
+            thresholds=sensors[key]['thresholds'][1..-2].split(/, /).map { |s| s.to_i }        
+            if (display)    
+              puts "#{key} #{t}#{Symbols.degree}C \t"+ Printer.gen_sparkline(t, thresholds)+" #{sensors[key]['name']}"
+            else
+              puts "#{key} #{sensors[key]['name']} temp: #{t}#{Symbols.degree}C  " + Printer.gen_sparkline(t, thresholds)
+            end
           end
         }
       end
