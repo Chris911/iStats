d02b4c0b9039b7d65dcad72d324d5c8f6358d552
Matti-Oskari Leppanen <mongrelx@gmail.com>
2016-10-14 08:13:27 +0000

    revert

--- ext/osx_stats/extconf.rb
+++ ext/osx_stats/extconf.rb
@@ -1,16 +1,4 @@
 require 'mkmf'
-require 'rbconfig'
-include RbConfig
-
-case CONFIG['host_os']
-  when /mswin|windows/i
-    # Windows
-  when /linux|arch/i
-  require 'iStats/linux_stats'
-         # Linux
-  when /sunos|solaris/i
-    # Solaris
-  when /darwin/i
     extension_name = 'osx_stats'
 
 CONFIG['LDSHARED'] << ' -framework IOKit -framework CoreFoundation '
@@ -21,7 +9,3 @@ dir_config(extension_name)
 # Do the work
 create_makefile(extension_name)
 
-    #MAC OS X
-  else
-    # whatever
-end
